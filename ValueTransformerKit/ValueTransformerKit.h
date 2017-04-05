4551718561b46384900ca35399a66c16c8de747f
Eric Marchand <eric.marchand@4d.com>
2017-04-05 16:55:32 +0200

    Initial Commit

---
+++ ValueTransformerKit/ValueTransformerKit.h
@@ -0,0 +1,19 @@
+//
+//  ValueTransformerKit.h
+//  ValueTransformerKit
+//
+//  Created by Eric Marchand on 05/04/2017.
+//  Copyright © 2017 Eric Marchand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ValueTransformerKit.
+FOUNDATION_EXPORT double ValueTransformerKitVersionNumber;
+
+//! Project version string for ValueTransformerKit.
+FOUNDATION_EXPORT const unsigned char ValueTransformerKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ValueTransformerKit/PublicHeader.h>
+
+
